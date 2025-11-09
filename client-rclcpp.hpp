7cdee2f7d77b11b743bc15e9677439eb1b94aea7
Christian Rauch <Rauch.Christian@gmx.de>
2025-11-09 04:09:27 +0100

    wrench

--- client-rclcpp.hpp
+++ client-rclcpp.hpp
@@ -1,32 +1,29 @@
 #pragma once
 #include "client.hpp"
+#include <geometry_msgs/msg/wrench.hpp>
 #include <qqml.h>
 #include <rclcpp/node.hpp>
-#include <mavros_msgs/msg/manual_control.hpp>
 
-
-class ClientROS : public Client
-{
-    Q_OBJECT
-    QML_ELEMENT
+class ClientROS : public Client {
+  Q_OBJECT
+  QML_ELEMENT
 public:
-    explicit ClientROS(QObject *parent = nullptr);
+  explicit ClientROS(QObject *parent = nullptr);
 
 private:
-    rclcpp::Node::SharedPtr node = nullptr;
-    rclcpp::Publisher<mavros_msgs::msg::ManualControl>::SharedPtr manual_control_pub = nullptr;
-    std::vector<std::string> argv_vec;
+  rclcpp::Node::SharedPtr node = nullptr;
+  rclcpp::Publisher<geometry_msgs::msg::Wrench>::SharedPtr manual_control_pub =
+      nullptr;
+  std::vector<std::string> argv_vec;
 
 public slots:
-    bool start(const QString &server) override;
+  bool start(const QString &server = {}) override;
 
-    bool stop() override;
+  bool stop() override;
 
-    bool
-    send(float roll, float pitch, float yaw, float throttle,
-         float aux1 = NAN, float aux2 = NAN, float aux3 = NAN,
-         float aux4 = NAN, float aux5 = NAN, float aux6 = NAN)  override;
+  bool send(float roll, float pitch, float yaw, float throttle,
+            float aux1 = NAN, float aux2 = NAN, float aux3 = NAN,
+            float aux4 = NAN, float aux5 = NAN, float aux6 = NAN) override;
 
 signals:
-
 };
